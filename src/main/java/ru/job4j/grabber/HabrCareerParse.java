bf006d48b3be8ac92cd8d561654a5fc416ed56b1
BazarnovSergey <ssergey101@yandex.ru>
2022-05-09 20:29:58 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,14 +5,25 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.HarbCareerDateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     /**
      * Метод для загрузки деталей объявления
@@ -27,26 +38,47 @@ public class HabrCareerParse {
         return elementsDescription.text();
     }
 
-    public static void main(String[] args) throws IOException {
-        HabrCareerParse habrCareerParse = new HabrCareerParse();
+    /**
+     * Метод создает объект Post с заполненными полями
+     * @param row элемент вакансии типа Element
+     * @return возваращает объект типа Post
+     */
+    private Post getPost(Element row) throws IOException {
+        Element titleElement = row.select(".vacancy-card__title").first();
+        Element linkElement = titleElement.child(0);
+        Element dateTitle = row.select(".vacancy-card__date").first().child(0);
+        String vacancyName = titleElement.text();
+        String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+        LocalDateTime localDateTime = dateTimeParser.parse(dateTitle.attr("datetime"));
+        return new Post(vacancyName, link, retrieveDescription(link), localDateTime);
+    }
+
+    /**
+     * Метод list загружает список всех постов
+     * @param link ссылка на страницу для парсинга
+     * @return возвращает список объектов типа Post
+     * @throws IOException
+     */
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> postsList = new ArrayList<>();
         for (int i = 1; i <= 5; i++) {
-            Connection connection = Jsoup.connect(PAGE_LINK + "?page=" + i);
+            Connection connection = Jsoup.connect(link + "?page=" + i);
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
             for (Element row : rows) {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
-                Element dateElement = row.select(".vacancy-card__date").first();
-                Element dateTimeElement = dateElement.child(0);
-                String vacancyName = titleElement.text();
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                String description = habrCareerParse.retrieveDescription(link);
-                System.out.println("Название вакансии : " + vacancyName
-                        + "\n" + "Дата : " + dateTimeElement.attr("datetime")
-                        + "\n" + "Ссылка : " + link
-                        + "\n" + "Описание : " + description);
-                System.out.println("----------------------------");
+                postsList.add(getPost(row));
             }
+        }
+        return postsList;
+    }
+
+    public static void main(String[] args) throws IOException {
+        HarbCareerDateTimeParser parser = new HarbCareerDateTimeParser();
+        HabrCareerParse hcp = new HabrCareerParse(parser);
+        List<Post> list = hcp.list(PAGE_LINK);
+        for (Post post : list) {
+            System.out.println(post.toString());
         }
     }
 }
--- src/main/java/ru/job4j/grabber/utils/HarbCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HarbCareerDateTimeParser.java
@@ -5,7 +5,12 @@ import java.time.LocalDateTime;
 import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
 
 public class HarbCareerDateTimeParser implements DateTimeParser {
-
+    /**
+     *  Метод, преобразующий дату из формата career.habr.com
+     *  в формат LocalDateTime
+     * @param parse дата полученная в результате парсинга
+     * @return возвращает объект типа LocalDateTime
+     */
     @Override
     public LocalDateTime parse(String parse) {
         return LocalDateTime.parse(parse, ISO_DATE_TIME);
